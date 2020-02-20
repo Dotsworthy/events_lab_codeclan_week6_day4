cdf9a842a147de18a9a597b1a0f1ba1b49dabc8a
Dotsworthy <dotsworthy@gmail.com>
2020-02-20 15:14:50 +0000

    completed MVP

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
+
+  const form = document.querySelector('#new-item-form');
+  form.addEventListener('submit', handleForm)
 })
+
+const handleForm = function() {
+  event.preventDefault();
+  const resultSubmit = document.querySelector('#reading-list');
+  const newListItem = document.createElement('li')
+  newListItem.textContent = `Title: ${event.target.title.value}, Author: ${event.target.author.value}, Category: ${event.target.category.value}`
+  const list = document.querySelector('ul');
+  list.appendChild(newListItem);
+
+  document.querySelector('#new-item-form').reset();
+};
